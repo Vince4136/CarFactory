b3cfc6319843a2bcefad7a6cca312b00fd5da036
Vincent Thompson <lycussuoe@gmail.com>
2022-10-07 11:17:21 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const addBody = (chassisObject) => {
+	// Use dot notation to add a new property
+	chassisObject.body = "Fever"
+
+	// Return the chassis that now has a body property on it
+	return chassisObject
+}
+
+const addwheels = (chassisObject, wheels) => {
+    chasisbody.wheels = 
